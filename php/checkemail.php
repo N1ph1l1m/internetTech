930a7463aa485f09a268ab3e4a42dffc9a12fe89
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2024-01-10 14:04:19 +0200

    Creake folder php and add files for server

---
+++ php/checkemail.php
@@ -0,0 +1,34 @@
+<?php
+
+header("Access-Control-Allow-Origin: *"); //add this CORS header to enable any domain to send HTTP requests to these endpoints:
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$conn = new mysqli("localhost","root","","reactjsdb");
+if(mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit();
+}else{
+    $eData = file_get_contents("php://input");
+    $dData = json_decode($eData,true);
+
+    $email = $dData['email'];
+    $result = "";
+
+    if($email != ""){
+        $sql = "SELECT * FROM user WHERE email = '$email';";
+        $res = mysqli_query($conn,$sql);
+
+        if(mysqli_num_rows($res) != 0){
+            $result = "Email is already taken";
+        }else{
+            $result = "";
+        }
+    }
+
+    $conn -> close();
+    $response[] = array("result" => $result);
+    echo json_encode($response); 
+}
+
+?>
